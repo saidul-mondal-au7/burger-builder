0fffdb8a1f580352b1d44d1c453c05baa5522041
Saidul Mondal <mondalsaidul3232@gmail.com>
2021-03-13 19:31:05 +0530

    burger-builder

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Layout from './components/layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
+
+function App() {
+  return (
+    <div >
+      <Layout>
+         <BurgerBuilder />
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
